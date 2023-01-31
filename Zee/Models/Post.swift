5e25264d7f7ada1ce206435b4ce6342e95dedb64
Trastoan <yurisaboiaf@gmail.com>
2023-01-31 18:21:08 -0300

    added star icon to UI and isFavorite property to model

--- Zee/Models/Post.swift
+++ Zee/Models/Post.swift
@@ -12,4 +12,14 @@ struct Post: Codable {
     var id: Int
     var title: String
     var body: String
+    var isFavorite: Bool
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.userId = try container.decode(Int.self, forKey: .userId)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.body = try container.decode(String.self, forKey: .body)
+        self.isFavorite = false
+    }
 }
--- Zee/Modules/PostList/View/PostListTableViewCell.swift
+++ Zee/Modules/PostList/View/PostListTableViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class PostListTableViewCell: UITableViewCell {
 
-    let postTitleLabel: UILabel = {
+    private let postTitleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
@@ -17,15 +17,29 @@ class PostListTableViewCell: UITableViewCell {
         return label
     }()
 
+    private let starIcon: UIImageView = {
+        let icon = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: 40, height: 40)))
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.contentMode = .scaleAspectFit
+        icon.image = UIImage(systemName: "star.fill")
+        icon.tintColor = .systemYellow
+        icon.isHidden = true
+        return icon
+    }()
+
+    private var leadingLabelAnchor: NSLayoutConstraint?
+
     override func prepareForReuse() {
         super.prepareForReuse()
         self.postTitleLabel.text = ""
+        adjustConstraintForFavorite(false)
     }
 
     func setup(_ post: Post) {
         self.selectionStyle = .none
         self.accessoryType = .disclosureIndicator
         postTitleLabel.text = post.title.capitalizedFirstLetter
+        adjustConstraintForFavorite(post.isFavorite)
     }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -38,16 +52,38 @@ class PostListTableViewCell: UITableViewCell {
         fatalError("No interface builder is used")
     }
 
+    private func adjustConstraintForFavorite(_ isFavorite: Bool) {
+        starIcon.isHidden = !isFavorite
+        //Remove old constraint
+        leadingLabelAnchor?.isActive = false
+        postTitleLabel.removeConstraint(leadingLabelAnchor!)
+        //Add new one
+        if isFavorite {
+            leadingLabelAnchor = postTitleLabel.leadingAnchor.constraint(equalTo: starIcon.trailingAnchor, constant: 8)
+        } else {
+            leadingLabelAnchor = postTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 18)
+        }
+        leadingLabelAnchor?.isActive = true
+    }
+
     private func setupSubviews() {
         contentView.addSubview(postTitleLabel)
+        contentView.addSubview(starIcon)
     }
 
     private func setupConstraints() {
+        leadingLabelAnchor = postTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 18)
+
         NSLayoutConstraint.activate([
-            postTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 18),
+            leadingLabelAnchor!,
             postTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -18),
             postTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             postTitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+
+            starIcon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            starIcon.widthAnchor.constraint(equalToConstant: 20),
+            starIcon.heightAnchor.constraint(equalToConstant: 20),
+            starIcon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 18),
         ])
     }
 }
